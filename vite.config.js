fccbac558eba3b904158804f894f6eee6a39bee8
roni5604 <roniro5604@gmail.com>
2025-10-07 18:58:06 +0300

    fix cname

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/matan-refaely/',
+  base: '/',      
   plugins: [react()],
 })
